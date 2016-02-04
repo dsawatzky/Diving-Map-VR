5e33f5a454f45d7daf5d51dea572be01b20067a5
Daniel Schumacher <schumi.daniel@gmx.net>
2016-02-04 15:42:46 +0100

    Display sound guide

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour
+{
+
+    public Texture2D image;
+    public Renderer rend;
+    public bool visible = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        image = (Texture2D)Resources.Load("sound_guide");
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("m"))
+        {
+            visible = !visible;
+        }    
+    }
+
+    void OnGUI()
+    {
+		if (visible == false) Graphics.DrawTexture(new Rect(170, 80, image.width / 2, image.height / 2), image);
+    }
+}
